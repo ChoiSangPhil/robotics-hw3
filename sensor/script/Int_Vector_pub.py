cebdb54a1118aa8bea3939d6e413cef5d5dd5ba1
Phil <Phil@pi.kr>
2020-11-19 12:30:18 +0900

    sensor에서의 TypeError와 NameError를 수정

--- sensor/script/Int_Vector_pub.py
+++ sensor/script/Int_Vector_pub.py
@@ -6,11 +6,13 @@ from common_msgs.msg import Int_Vector
 rospy.init_node('Int_Vector_pub')
 pub = rospy.Publisher('hw3_topic_msg', Int_Vector, queue_size=1)
 msg = Int_Vector()
+msg.num = 1
 rate = rospy.Rate(2)
 
 while not rospy.is_shutdown():
-    msg.num = rospy.get_rostime() % 9 + 1
-    msg.vec = Vector3(x=num, y=num*2, z=num**2)
+    basis = msg.num
+    msg.vec = Vector3(x=basis, y=basis*2, z=basis**2)
     pub.publish(msg)
     print "publish: ", msg.vec.x, msg.vec.y, msg.vec.z
+    msg.num += 1
     rate.sleep()
