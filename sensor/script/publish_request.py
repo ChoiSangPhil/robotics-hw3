608ca2b5c090a7187aa6a81fc8654ac2f45bc864
Phil <Phil@pi.kr>
2020-11-20 03:34:58 +0900

    일정한 count값에서 입력받은 A와 B를 XOR하도록 서비스 요청을 통해 알리는 것을 구현

--- sensor/script/publish_request.py
+++ sensor/script/publish_request.py
@@ -4,11 +4,14 @@ import random
 import math
 from geometry_msgs.msg import Vector3
 from common_msgs.msg import Coordinate
+from common_msgs.srv import XORGate, XORGateRequest
 
-rospy.init_node('Coordinate_pub')
+rospy.init_node('msgs_publish')
+requester = rospy.ServiceProxy('xor_gate', XORGate)
 pub = rospy.Publisher('hw3_topic_msg', Coordinate, queue_size=1)
 msg = Coordinate()
 rate = rospy.Rate(2)
+count = 0
 
 while not rospy.is_shutdown():
     msg.radius = random.randint(1,10)
@@ -19,5 +22,10 @@ while not rospy.is_shutdown():
     msg.vec = Vector3(x=r_cos, y=r_sin, z=height)
     print "publish: ", msg.radius, msg.vec.x, msg.vec.y, msg.vec.z
     pub.publish(msg)
+    if count % 10 == 0:
+        req = XOR_gateRequest(A=random.randint(0,1), B=random.randint(0,1))
+        res = requester(req)
+        print count, "request:", req.A, req.B, "response:", res.F
     rate.sleep()
+    count += 1
 
