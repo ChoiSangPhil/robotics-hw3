f90ed74f7cafbb18144b35970b38305ffaf761ef
Phil <Phil@pi.kr>
2020-11-19 22:20:04 +0900

    원통좌표계 구현

--- algorithm/script/subscribe_response.py
+++ algorithm/script/subscribe_response.py
@@ -3,7 +3,7 @@ import rospy
 from common_msgs.msg import Coordinate
 
 def callback(msg):
-    print "subscribe: ", msg.vec.x, msg.vec.y, msg.vec.z
+    print "subscribe: ", msg.radius, msg.vec.x, msg.vec.y, msg.vec.z
 
 rospy.init_node('Coordinate_sub')
 sub = rospy.Subscriber('hw3_topic_msg', Coordinate, callback)
