b11c2a4ba007a1363364a65bf3765e088c52bfa0
Phil <Phil@pi.kr>
2020-11-19 12:37:24 +0900

    algorithm에서의 ImportError와 NameError를 수정

--- algorithm/script/Int_Vector_sub.py
+++ algorithm/script/Int_Vector_sub.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
-
 import rospy
-from common_msgs.msg import Int_Vecter
+from common_msgs.msg import Int_Vector
 
 def callback(msg):
     print "subscribe: ", msg.vec.x, msg.vec.y, msg.vec.z
 
 rospy.init_node('Int_Vecter_sub')
-sub = rospy.Subscriber('hw3_topic_msg', Int_Vecter, callback)
+sub = rospy.Subscriber('hw3_topic_msg', Int_Vector, callback)
 rospy.spin()
 
