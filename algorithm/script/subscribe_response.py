e20636e0b43b2a3f21bcf87de271bc601f0bf5d4
Phil <Phil@pi.kr>
2020-11-19 20:48:06 +0900

    common_msgs 수정에 따른 algorithm 파일명, 변수명 변경

---
+++ algorithm/script/subscribe_response.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import rospy
+from common_msgs.msg import Coordinate
+
+def callback(msg):
+    print "subscribe: ", msg.vec.x, msg.vec.y, msg.vec.z
+
+rospy.init_node('Coordinate_sub')
+sub = rospy.Subscriber('hw3_topic_msg', Coordinate, callback)
+rospy.spin()
+
